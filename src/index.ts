69e9c79bdb8389f9d4c8a64243aa3ceb7d2d072d
quirogatdf1 <104772903+quirogatdf1@users.noreply.github.com>
2022-05-26 09:42:52 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+const btnIniciar = document.getElementById("btnIniciar");
+let mostrarDatos = document.getElementById("mostrarDatos");
+
+btnIniciar.addEventListener("click", () => {
+  const cantidadAlumnos: number = Number(
+    prompt("Ingrese la cantidad de alumnos")
+  );
+  /* Se definen las variables */
+  let arrayNombreAlumno: string[] = new Array(cantidadAlumnos);
+  let arrayNota1: number[] = new Array(cantidadAlumnos);
+  let arrayNota2: number[] = new Array(cantidadAlumnos);
+  let arrayNota3: number[] = new Array(cantidadAlumnos);
+  let arrayPromedioAnual: number[] = new Array(cantidadAlumnos);
+
+  /* Se cargan los datos en sus respectivos arreglos */
+  for (let i = 0; i < cantidadAlumnos; i++) {
+    arrayNombreAlumno[i] = prompt("Ingrese el nombre del Estudiante");
+    arrayNota1[i] = Number(
+      prompt(`Ingrese la primer nota del Estudiante ${arrayNombreAlumno[i]}`)
+    );
+    arrayNota2[i] = Number(
+      prompt(`Ingrese la segunda nota del Estudiante ${arrayNombreAlumno[i]}`)
+    );
+    arrayNota3[i] = Number(
+      prompt(`Ingrese la tercera nota del Estudiante ${arrayNombreAlumno[i]}`)
+    );
+    arrayPromedioAnual[i] = (arrayNota1[i] + arrayNota2[i] + arrayNota3[i]) / 3;
+    /* Muestra los resultados en la página HTML */
+    mostrarDatos.innerHTML += `El promedio anual del estudiante ${arrayNombreAlumno[i]}, es ${arrayPromedioAnual[i]} <br>`;
+  }
+});
